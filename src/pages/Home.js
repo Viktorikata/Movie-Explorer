8ae046cfebd2433ab0ad63764f3fb6bc7e695d79
Viktoria <va.2816275@yandex.com>
2025-07-06 10:32:02 +0300

    Создание основной структуры, Home.js, api.js

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import {fetchPopularMovies} from "../services/api";
+
+function Home() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const loadMovies = async () => {
+            const data = await fetchPopularMovies;
+            setMovies(data);
+        };
+
+        loadMovies();
+    }, []);
+
+    return (
+        <div style={{padding: "20px"}}>
+            <h1>Популярные фильмы</h1>
+            <div style={{display:"flex", flexWrap: "wrap", gap: "20px"}}>
+                {movies.map(movie => (
+                <div key={movie.id} style={{width:"20px"}}>
+                <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                    style={{width: "100%"}}
+                />
+                <h3>{movie.title}</h3>
+            </div>
+            ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = process.env.React_App_TMBD_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchPopularMovies = async () => {
+    try {
+        const response = await fetch (
+            `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ru-RU&page=1`
+        );
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error ('Ошибка при получении фильмов:', error);
+        return [];
+    }
+};
