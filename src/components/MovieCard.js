5ce700930b043c3ecc6647d4cc60ec0b6b6c6b9c
Viktoria <va.2816275@yandex.com>
2025-07-10 20:59:50 +0300

    Добавлены стили для карточек

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './MovieCard.css';
 
 function MovieCard ({movie}) {
     return (
-        <div style={{width: "200px"}}>
+        <div className="movie-card" style={{width: "200px"}}>
             <img 
                 src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
                 alt={movie.tittle}
                 style={{width:'100%', borderRadius: '10px'}}
             />
+            <div className='movie-card-content'>
             <h3>{movie.title}</h3>
+            </div>
         </div>
     );
 }
